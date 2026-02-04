15704277a5f49b42d1178ecafb8dce39359e133b
unknown <you@example.com>
2026-02-04 09:39:57 +0700

    membuatClassDanCafe

--- LATIHAN_2407067/src/latihan_PBO/latihan1/main.java
+++ LATIHAN_2407067/src/latihan_PBO/latihan1/main.java
@@ -1,7 +1,26 @@
 package latihan_PBO.latihan1;
 
+
+class Cafe{
+    String Nama;
+    String Alamat;
+}
+
+//kelas main adalah untuk menampilkan program yang akan kita buat
 public class main {
     public static void main(String[] args) {
-        System.out.println("praktikum1");
+        Cafe cafe1 = new Cafe();
+        Cafe cafe2 = new Cafe();
+        cafe1.Nama = "Kopi Kenangan";
+        cafe1.Alamat = "Indramayu";
+        cafe2.Nama = "kopte";
+        cafe2.Alamat = "IMKOT";
+
+        System.out.println("============cafe1===============");
+        System.out.println("nama cafenya adalah" + cafe1.Nama);
+        System.out.println("alamatnya ada di kabupaten: " + cafe1.Alamat);
+        System.out.println("============cafe2===============");
+        System.out.println("nama cafenya adalah " + cafe2.Nama);
+        System.out.println("alamatnya ada di daerah: " + cafe2.Alamat);
     }
 }
