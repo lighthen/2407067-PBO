6dcba85e2200fab41feb48120590a93ed13ea831
unknown <you@example.com>
2026-02-05 10:56:24 +0700

    latihan1

--- LATIHAN_2407067/src/latihan_PBO/latihan1/latihan_PBO/latihan1/main.java
+++ LATIHAN_2407067/src/latihan_PBO/latihan1/latihan_PBO/latihan1/main.java
@@ -11,13 +11,22 @@ class Cafe{
         System.out.println("nama cafenya adalah " + this.Nama);
         System.out.println("alamatnya ada di daerah: " + this.Alamat);
     }
+    void buka() {
+        System.out.println(this.Nama + " cafe sekarang sudah buka yaa");
+    }
 }
 
 //kelas main adalah untuk menampilkan program yang akan kita buat
 public class main {
     public static void main(String[] args) {
-        Cafe cafe1 = new Cafe("janjijiwa", "indramayu");
-        Cafe cafe2 = new Cafe("kopte", "imkot");
+        System.out.println("HENDRA NURFATKHURROHMAN");
+        System.out.println("============cafe1===============");
+        Cafe cafe1 = new Cafe("janjijiwa", "bayalangu");
+        cafe1.buka(); 
+        System.out.println("============cafe2===============");
+        Cafe cafe2 = new Cafe("kopte", "gegesik");
+        cafe2.buka();
+        System.out.println("============thankYou===============");
         // cafe1.Nama = "Kopi Kenangan";
         // cafe1.Alamat = "Indramayu";
         // cafe2.Nama = "kopte";
@@ -26,7 +35,7 @@ public class main {
         // System.out.println("============cafe1===============");
         // System.out.println("nama cafenya adalah" + cafe1.Nama);
         // System.out.println("alamatnya ada di kabupaten: " + cafe1.Alamat);
-        // System.out.println("============cafe2===============");
+        // 
         // System.out.println("nama cafenya adalah " + cafe2.Nama);
         // System.out.println("alamatnya ada di daerah: " + cafe2.Alamat);
     }
